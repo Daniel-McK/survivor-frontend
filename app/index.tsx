8d0914dbb23ce492ce8fca343257ab1dbbc4e981
Daniel McKittrick <mckittrickdaniel@gmail.com>
2018-09-04 20:15:53 -0400

    Add theme colors

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,6 @@
 import * as ReactDOM from 'react-dom';
 import * as React from 'react';
-import './index.scss';
+import './styles/index.scss';
 import App from './scripts/components/general/App';
 
 ReactDOM.render(
