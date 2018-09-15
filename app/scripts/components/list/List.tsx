354d46672efe65872273e440cd9e098eb9fab6e8
Daniel McKittrick <mckittrickdaniel@gmail.com>
2018-09-15 15:53:40 -0400

    Scrollable contestant

--- app/scripts/components/list/List.tsx
+++ app/scripts/components/list/List.tsx
@@ -2,6 +2,12 @@ import * as React from 'react';
 import { css } from 'emotion';
 import styled from 'react-emotion';
 import theme from '../../../styles/theme';
+import { replace } from 'lodash';
+
+const ListScroller = styled('div')`
+  overflow-x: auto;
+  max-width: 100%;
+`;
 
 const ListWrapper = styled('ul')`
   list-style: none;
@@ -9,20 +15,34 @@ const ListWrapper = styled('ul')`
   padding: 0;
   display: flex;
   border-bottom: solid 1px #c6ddde;
-  justify-content: center;
 `;
 
 const ListItem = styled('li')(({ selected }: any) => `
-  padding: 10px 15px;
-  background-color: ${selected && theme.color.primaryLight};
-  color: ${selected ? theme.color.primaryFont : theme.color.secondaryFont};
   cursor: pointer;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  margin: 10px 5px;
 
-  &:hover {
+  &:hover > pre {
     background-color: ${selected ? theme.color.primaryLight : '#c6ddde'}
   }
 `
 );
+
+const FloatingName = styled('pre')(({ selected }: any) => `
+  position: absolute;
+  margin: 0;
+  padding: 5px 0;
+  bottom: 0;
+  text-align: center;
+  vertical-align: center;
+  background-color: ${selected ? theme.color.primaryLight : '#F5FFFF'};
+  color: ${selected ? theme.color.primaryFont : theme.color.secondaryFont};
+  width: 100px;
+  opacity: 0.9;
+`);
 
 interface ListProps {
   options: any[];
@@ -36,17 +56,20 @@ export class List extends React.Component<ListProps, {}> {
     const { active, displayProperty, onClick, options } = this.props;
 
     return (
-      <ListWrapper>
-        {options.map((option, index) => (
-          <ListItem
-            onClick={onClick.bind(null, option)}
-            key={option[displayProperty]}
-            selected={active === option}
-          >
-            {option[displayProperty]}
-          </ListItem>
-        ))}
-      </ListWrapper>
+      <ListScroller>
+        <ListWrapper>
+          {options.map((option, index) => (
+            <ListItem
+              onClick={onClick.bind(null, option)}
+              key={option[displayProperty]}
+              selected={active === option}
+            >
+              <img src={option.imageUrl} width="100" height="125" />
+              <FloatingName selected={active === option}>{replace(option[displayProperty], ' ', '\r\n')}</FloatingName>
+            </ListItem>
+          ))}
+        </ListWrapper>
+      </ListScroller>
     );
   }
 }
