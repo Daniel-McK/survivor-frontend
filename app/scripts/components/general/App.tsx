f07136188a2453bee8666b6960bcc699d2a16bb8
Daniel McKittrick <mckittrickdaniel@gmail.com>
2019-03-16 12:59:35 -0400

    Refactor data loading, use hooks in App.ts

--- app/scripts/components/general/App.tsx
+++ app/scripts/components/general/App.tsx
@@ -1,20 +1,14 @@
 import * as React from 'react';
-import { isEmpty } from 'lodash';
 import { Header } from './Header';
 import styled from 'react-emotion';
 import { AppTab } from '../../models/structure';
 import Contestants from '../contestants/Contestants';
 import { connect } from 'react-redux';
-import { DDBPoint, Contestant, Point, User } from '../../models/types';
-import { createLoadPointsAction } from '../../store/actions/points';
-import { getPoints, getContestants, getUsers } from '../../api/api-gateway';
-import {
-  createLoadContestantsAction,
-  createRankContestantsAction
-} from '../../store/actions/contestants';
+import { Contestant, Point, User } from '../../models/types';
 import { ReduxState } from '../../store';
-import { createLoadUsersAction } from '../../store/actions/users';
 import { Users } from '../users/Users';
+import { Dispatch } from 'redux';
+import { loadAllContent } from '../../store/async/data';
 
 const Scrollable = styled('div')`
   overflow-y: auto;
@@ -27,94 +21,32 @@ interface AppProps {
   users?: User[];
 
   // redux actions
-  loadPoints?: (points: DDBPoint[]) => void;
-  loadContestants?: (contestants: Contestant[]) => void;
-  loadUsers?: (users: User[]) => void;
-  rankContestants?: (contestants: Contestant[], points: Point[]) => void;
+  dispatch?: Dispatch;
 }
 
-interface AppState {
-  selectedTab: AppTab;
-}
-
-class App extends React.Component<AppProps, AppState> {
-  constructor(props: AppProps) {
-    super(props);
-    this.state = {
-      selectedTab: AppTab.Contestants
-    };
-  }
-
-  public componentDidMount() {
-    this.fetchContestants();
-    this.fetchPoints();
-    this.fetchUsers();
-  }
-
-  public componentWillReceiveProps(newProps: AppProps) {
-    if (!this.hasLoaded(this.props) && this.hasLoaded(newProps)) {
-      this.props.rankContestants(newProps.contestants, newProps.points);
-    }
-  }
-
-  public render() {
-    return (
-      <main>
-        <Header
-          onTabChange={this.updateSelectedTab}
-          selectedTab={this.state.selectedTab}
-        />
-        <Scrollable>{this.renderMain()}</Scrollable>
-      </main>
-    );
-  }
-
-  private renderMain = () => {
-    switch (this.state.selectedTab) {
-      // case AppTab.Episodes:
-      //   return <Episodes />;
-      case AppTab.Users:
-        return <Users users={this.props.users} />;
-      case AppTab.Contestants:
-      default:
-        return (
+const App: React.SFC<AppProps> = props => {
+  const [selectedTab, setSelectedTab] = React.useState(AppTab.Contestants);
+  React.useEffect(() => {
+    loadAllContent(props.dispatch);
+  }, []);
+  return (
+    <main>
+      <Header
+        onTabChange={setSelectedTab}
+        selectedTab={selectedTab}
+      />
+      <Scrollable>
+        {selectedTab === AppTab.Users && <Users users={props.users} />}
+        {selectedTab === AppTab.Contestants && (
           <Contestants
-            contestants={this.props.contestants}
-            users={this.props.users}
+            contestants={props.contestants}
+            users={props.users}
           />
-        );
-    }
-  };
-
-  private updateSelectedTab = (selectedTab: AppTab) => {
-    this.setState({
-      selectedTab
-    });
-  };
-
-  private fetchPoints = async () => {
-    const response = await getPoints('season-38');
-    this.props.loadPoints(response.data);
-  };
-
-  private fetchContestants = async () => {
-    const response = await getContestants('season-38');
-    this.props.loadContestants(response.data);
-  };
-
-  private fetchUsers = async () => {
-    const response = await getUsers();
-    this.props.loadUsers(response.data);
-  };
-
-  private hasLoaded = (props: AppProps) => {
-    return (
-      !isEmpty(props.contestants) &&
-      !isEmpty(props.points) &&
-      !isEmpty(props.users)
-    );
-  };
-}
+        )}
+      </Scrollable>
+    </main>
+  );
+};
 
 function mapStateToProps(state: ReduxState) {
   return {
@@ -124,19 +56,4 @@ function mapStateToProps(state: ReduxState) {
   };
 }
 
-function mapDispatchToProps(dispatch) {
-  return {
-    loadContestants: (contestants: Contestant[]) =>
-      dispatch(createLoadContestantsAction(contestants)),
-    loadPoints: (points: DDBPoint[]) =>
-      dispatch(createLoadPointsAction(points)),
-    loadUsers: (users: User[]) => dispatch(createLoadUsersAction(users)),
-    rankContestants: (contestants: Contestant[], points: Point[]) =>
-      dispatch(createRankContestantsAction(contestants, points))
-  };
-}
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(App);
+export default connect(mapStateToProps)(App);
---
+++ app/scripts/store/async/data.ts
@@ -0,0 +1,36 @@
+import { Dispatch } from 'redux';
+import { getPoints, getContestants, getUsers } from '../../api/api-gateway';
+import { createRankContestantsAction, createLoadContestantsAction } from '../actions/contestants';
+import { createLoadUsersAction } from '../actions/users';
+import { createLoadPointsAction } from '../actions/points';
+
+async function loadPoints(dispatch: Dispatch) {
+  const response = await getPoints('season-38');
+  dispatch(createLoadPointsAction(response.data));
+  return response.data;
+}
+
+async function loadContestants(dispatch: Dispatch) {
+  const response = await getContestants('season-38');
+  dispatch(createLoadContestantsAction(response.data));
+  return response.data;
+}
+
+async function loadUsers(dispatch: Dispatch) {
+  const response = await getUsers();
+  dispatch(createLoadUsersAction(response.data));
+  return response.data;
+}
+
+export async function loadAllContent(dispatch: Dispatch) {
+  const [
+    points,
+    users,
+    contestants
+  ] = await Promise.all([
+    loadPoints(dispatch),
+    loadUsers(dispatch),
+    loadContestants(dispatch)
+  ]);
+  dispatch(createRankContestantsAction(contestants, points));
+}
